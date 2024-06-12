5f71a3bc6ac8ad89f7f335ce1a921ebe6ea6f059
sanower <sanower@emmes.com>
2024-06-12 19:04:45 +0530

    Added all the changes for seen of messages in real time

--- client/src/hooks/useListenMessages.js
+++ client/src/hooks/useListenMessages.js
@@ -13,9 +13,29 @@ const useListenMessages = () => {
       const sound = new Audio(notificationSound);
       newMessage.status = "SEEN";
       setMessages([...messages, newMessage]);
+      // // todo: set this newMessage as seen for other user through socket
+      setTimeout(() => {
+        socket.emit("newMessageSeen", { ...newMessage });
+      }, 2000);
       sound.play();
     });
-    return () => socket?.off("newMessage");
+
+    socket?.on("newMessageSeen", ({ messageId }) => {
+      const filteredMessages = messages.filter(
+        (eachMessage) => eachMessage?._id !== messageId
+      );
+      const messageToFind = messages.find(
+        (eachMessage) => eachMessage?._id === messageId
+      );
+      messageToFind.status = "SEEN";
+      setMessages([...filteredMessages, messageToFind]);
+      console.log("messages inside newMessageSeen: ", messages);
+    });
+
+    return () => {
+      socket?.off("newMessage");
+      socket?.off("newMessageSeen");
+    };
   }, [socket, messages, setMessages]);
 };
 
